6f1e772355258ee343d7990a39572146fe1b765e
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2020-05-19 17:03:00 +0530

    Added dense flow support

--- script.py
+++ script.py
@@ -14,10 +14,15 @@ from imutils.video import FPS
 vs = WebcamVideoStream(src=0).start()
 fps = FPS().start()
 W, H = 224, 224
-saved_path = 'URFD_images1/'
-
+saved_path = 'URFD_images/'
+output_path = 'URFD_opticalflow/'
 if not os.path.exists(saved_path):
 	os.makedirs(saved_path)
+
+if not os.path.exists(output_path):
+	os.makedirs(output_path)
+
+
 
 while True:
     frame = vs.read()
@@ -25,3 +30,7 @@ while True:
     dateTimeObj = datetime.now()
     timestampStr = dateTimeObj.strftime("%d:%m:%Y;%H:%M:%S.%f") 
     cv2.imwrite(saved_path + str(timestampStr) + '.jpg',cv2.resize(frame, (W,H)),[int(cv2.IMWRITE_JPEG_QUALITY), 95])
+    path = saved_path + '/'
+	#vid_file = data_folder + activity_folder  + '/Videos/' + video_folder.replace(' ','')
+    flow = output_path + '/'
+    os.system('dense_flow2/build/extract_cpu -f={} -x={} -y={} -i=tmp/image -b=20 -t=1 -d=0 -s=1 -o=dir'.format(path, flow + '/flow_x', flow + '/flow_y'))
